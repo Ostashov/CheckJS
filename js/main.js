cb712a82d657cf83d4bcb21807951ccece9fbe2c
Ostashov <oleg.ostashov@gmail.com>
2013-03-05 16:50:54 +0400

    Add JSON (not work)

--- js/main.js
+++ js/main.js
@@ -1,32 +1,30 @@
-function TestProb (code, data, answer) {
+﻿function TestProblem (tests, code) {
 	var user_answer =[];
 	var result = [];
-	eval (code);
+	eval(code);
 	for (var i = 0; i < 4; i++) {
-		if (sum(data[i]) === answer[i]) {
+		if (sum(tests[i].data) === tests[i].answer) {
 			result[i] = 'OK';
 		} else {
 			result[i] = 'NO';
 		}
-		user_answer[i] = sum(data[i]);
+		user_answer[i] = sum(tests[i].data);
 	}
 	return {"result":result, "user_answer":user_answer};
 }
 
 
 var button = document.getElementsByTagName('input')[0];
+var id = 1;
 button.onclick = function() {
 	var code = document.getElementsByName('content')[0].value;
-	var data = ['11', '990', '1', '1010101'];
-	var answer = [2, 18, 1, 4];
-
-	var report = TestProb(code, data, answer);
-	PrintResultsTable(data, answer, report);
+	var report = TestProblem(problems[id].tests, code);
+	PrintResultsTable(problems[id].tests, report);
 	
 	return false;
 }
 
-function PrintResultsTable(data, answer, report) {
+function PrintResultsTable(tests, report) {
 	var table = document.getElementsByTagName('table')[0];
 	table.setAttribute('border', '1');
 	table.innerHTML = "<tr>\
@@ -39,9 +37,9 @@ function PrintResultsTable(data, answer, report) {
 		for (i = 0; i < 4; i++) {
 			table.innerHTML = table.innerHTML + "<tr>\
 		<td>" + (i+1) + "</td>\
-		<td>" + data[i] + "</td>\
+		<td>" + tests[i].data + "</td>\
 		<td>" + report.user_answer[i] + "</td>\
-		<td>" + answer[i] + "</td>\
+		<td>" + tests[i].answer + "</td>\
 		<td>" + report.result[i] + "</td>\
 		</tr>";
 		}
---
+++ js/problems/1-10.js
@@ -0,0 +1,6 @@
+﻿var problems = [];
+problems[1] = {
+	"tests": [{"data":11, "answer":2}, {"data":990, "answer":18}, {"data":1, "answer":1}, {"data":1010101, "answer":4}]
+	//"data": ['11', '990', '1', '1010101'],
+	//"answer": [2, 18, 1, 4]
+}
